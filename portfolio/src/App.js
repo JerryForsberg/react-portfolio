8c5e1837c7122641c50a31416575a3b75e9d8856
JerryForsberg <Jerryforsberg8991@gmail.com>
2020-09-13 14:16:15 -0700

    set up initial pages and components

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import PastWork from "./pages/PastWork";
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+
+      <Route exact path="/" component={About} />
+      <Route exact path="/contact" component={Contact} />
+      <Route exact path="/past_work" component={PastWork} />
+
+
+    </Router>
   );
 }
 
---
+++ portfolio/src/pages/PastWork.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PastWork () {
+    return (
+        <div>
+            <h1>Projects</h1>
+        </div>
+    );
+}
+
+export default PastWork;
